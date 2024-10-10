3309bae6b56314801e69f9ebac3ed9d39fc297af
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2024-10-11 00:37:52 +0330

    fix: x axis items

--- main.go
+++ main.go
@@ -19,8 +19,7 @@ import (
 	"github.com/mum4k/termdash/widgets/linechart"
 )
 
-var arr []float64
-var pid *int
+const resolution = 100
 
 func main() {
 	if len(os.Args) < 3 {
@@ -31,12 +30,19 @@ func main() {
 	pidArg := os.Args[1]
 	intervalArg := os.Args[2]
 	id, _ := strconv.Atoi(pidArg)
-	intervalMilli, _ := strconv.Atoi(intervalArg)
+	intervalArgInt, _ := strconv.Atoi(intervalArg)
 	pid = &id
+	interval := time.Duration(intervalArgInt) * time.Millisecond
 
-	for i := 0; i < 100; i++ {
-		arr = append(arr, 0)
+	currentTime := time.Now()
+	for i := resolution; i > 0; i-- {
+		pair := Pair{
+			value: 0,
+			time:  currentTime.Add(time.Duration(intervalArgInt*i) * time.Millisecond).Format("15:04:05"),
+		}
+		arr = append(arr, pair)
 	}
+
 	t, err := tcell.New()
 	if err != nil {
 		panic(err)
@@ -46,7 +52,6 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	// ----------------------------------------------
-	var interval = time.Duration(intervalMilli) * time.Millisecond
 	lc, err := linechart.New(
 		linechart.AxesCellOpts(cell.FgColor(cell.ColorRed)),
 		linechart.YLabelCellOpts(cell.FgColor(cell.ColorGreen)),
@@ -81,7 +86,15 @@ func main() {
 	}
 }
 
-func GetMemoryUsage(pid int) ([]float64, error) {
+type Pair struct {
+	value float64
+	time  string
+}
+
+var arr []Pair
+var pid *int
+
+func GetMemoryUsage(pid int) ([]Pair, error) {
 	statusFile := fmt.Sprintf("/proc/%d/status", pid)
 	file, err := os.Open(statusFile)
 	if err != nil {
@@ -97,11 +110,11 @@ func GetMemoryUsage(pid int) ([]float64, error) {
 			if len(fields) >= 2 {
 				// VmRSS value is in kB, convert to int
 				memKb, err := strconv.Atoi(fields[1])
-				if len(arr) > 100 {
-
-					arr = append(arr[1:], float64(memKb)/1024)
+				pair := Pair{value: float64(memKb) / 1024, time: time.Now().Format("04:05")}
+				if len(arr) > resolution {
+					arr = append(arr[1:], pair)
 				} else {
-					arr = append(arr, float64(memKb)/1024)
+					arr = append(arr, pair)
 				}
 
 				if err != nil {
@@ -125,13 +138,17 @@ func playLineChart(ctx context.Context, lc *linechart.LineChart, delay time.Dura
 	for {
 		select {
 		case <-ticker.C:
-			inputs, _ := GetMemoryUsage(*pid)
+			memRecs, _ := GetMemoryUsage(*pid)
+			var inputs []float64
+			var xItems map[int]string = make(map[int]string)
+			for i, v := range memRecs {
+				inputs = append(inputs, v.value)
+				xItems[i] = v.time
+			}
 
 			if err := lc.Series("first", inputs,
 				linechart.SeriesCellOpts(cell.FgColor(cell.ColorNumber(33))),
-				linechart.SeriesXLabels(map[int]string{
-					0: "zero",
-				}),
+				linechart.SeriesXLabels(xItems),
 			); err != nil {
 				panic(err)
 			}
